f75dcd9ad30f508b4f8e7151ae81dcefaa1a83a2
Florian Pechwitz <florian.pechwitz@itizzimo.com>
2017-05-12 11:58:51 +0200

    Added windows support

---
+++ src/windows/BuildInfoProxy.js
@@ -0,0 +1,19 @@
+cordova.define("cordova-plugin-buildinfo.BuildInfoProxy", function (require, exports, module) {
+    BuildInfoProxy = {
+        init: function (successCallback, errorCallback, args) {
+            var res = {};
+            var packId = Windows.ApplicationModel.Package.current.id;
+            res.packageName = packId.name;
+            res.basePackageName = packId.name;
+            res.displayName = Windows.ApplicationModel.Package.current.displayName;
+            res.name = Windows.ApplicationModel.Package.current.displayName;
+            var version = packId.version;
+            res.version = [version.major, version.minor, version.build].join('.');
+            res.versionCode = packId.version.build;
+            res.debug = Windows.ApplicationModel.Package.current.isDevelopmentMode;
+            res.buildType = (res.debug) ? "debug" : "release";
+            successCallback(res);
+        }
+    };
+    cordova.commandProxy.add("BuildInfo", BuildInfoProxy);
+});
